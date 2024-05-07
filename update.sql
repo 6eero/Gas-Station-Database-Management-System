4dae8ebab3453ca91d94c2e7da0c6a28a8c7c1d4
BenedettaS <138503833+BenedettaS@users.noreply.github.com>
2024-05-07 22:55:33 +0200

    Update update.sql

--- update.sql
+++ update.sql
@@ -207,7 +207,7 @@ codiceStazione1 dom_codice_stazione;
 BEGIN
 	SELECT count(DISTINCT codiceStazione) into numstazioni from PIANO_DI_LAVORO_GIORNALIERO AS PLG where PLG.CFDipendente=old.CFDipendente group by CFDipendente;
 	SELECT count(*) into pianistaz from PIANO_DI_LAVORO_GIORNALIERO AS PLG WHERE PLG.codiceStazione=old.codiceStazione and PLG.CFDipendente=old.CFDipendente group by CFDipendente,codiceStazione;
-	IF numstazioni<2 or (numstazioni=2 and pianistaz<2)
+	IF numstazioni<=2 or (numstazioni=2 and pianistaz<2)
 	THEN
 		RAISE NOTICE 'Il dipendente lavora in UNA SOLA stazione, trasferisci da tipo2 a tipo1';
 		PERFORM * from tipo2 where cf=old.cfdipendente;
@@ -262,7 +262,7 @@ BEGIN
 	SELECT count(DISTINCT codicestazione) into numstazioni from PIANO_DI_LAVORO_GIORNALIERO AS PLG where PLG.CFDipendente=new.CFDipendente group by CFDipendente;
 	SELECT count(*) into pianistaz from PIANO_DI_LAVORO_GIORNALIERO AS PLG WHERE PLG.codiceStazione=new.codiceStazione and PLG.CFDipendente=new.CFDipendente
 	group by CFDipendente,codiceStazione;
-	IF numstazioni<2 or (numstazioni=2 and pianistaz<2)
+	IF numstazioni<=2 or (numstazioni=2 and pianistaz<2)
 	THEN 
 		RAISE NOTICE 'Il dipendente lavora in UNA SOLA stazione, trasferisci da tipo2 a tipo1';
 		PERFORM * from tipo2 where cf=new.cfdipendente;
